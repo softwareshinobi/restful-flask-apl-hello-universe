8495056ec354b1735a2e23f2ee34bcbf784a5551
Software Shinobi <the.software.shinobi@gmail.com>
2024-11-23 07:43:16 -0500

    automated terminal push

--- provision.bash
+++ provision.bash
@@ -11,8 +11,13 @@ sudo apt update
 
 sudo apt install -y python3-pip
 
+sudo apt install -y python3-flask
+
+sudo apt install -y python3.12-venv
 
 python3 -m venv my_venv
+
+source my_venv/bin/activate
 
 pip install -r requirements.txt   
 
